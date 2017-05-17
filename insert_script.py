6f81922b9276b371e9210774c48a426ff45900f2
Pavlo Zhavoronkov <zloool@github.com>
2017-05-17 11:00:14 +0300

    Upgrade insert script

--- insert_script.py
+++ insert_script.py
@@ -8,13 +8,50 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from password import SQLALCHEMY_DATABASE_URI
-import app
+
+
+class LeaksIterator:
+    def __init__(self, filename):
+        self.leak_file = open(filename, "rU")
+        self.size = sum(1 for line in self.leak_file)
+        self.leak_file = open(filename, "rU")
+        self.cache = []
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return self.size
+
+    def next(self):
+        if self.leak_file.closed:
+            raise StopIteration
+        leaks = []
+        x = 0
+        # print "begin loop"
+        while x < 10000:
+            line = self.leak_file.readline()
+            if not line:
+                self.leak_file.close()
+                raise StopIteration
+            try:
+                #res = re.findall(r'.*\:(?P<email>.*@.*)\:.*\:(?P<pass>.*)', line)
+                res = re.findall(r'(?P<email>.*@.*)\:(?P<pass>.*)', line)
+                leaks.append(
+                    {'email': res[0][0], 'password_hash': res[0][1], 'leak_source': 'exploit_in'})
+                x += 1
+            except Exception as e:
+                pass
+        # print "end loop"
+        return leaks
+
 
 app = Flask(__name__)
 app.config.from_object("config")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
+
 
 class Leak(db.Model):
     __tablename__ = 'LEAKS_staging'
@@ -46,34 +83,33 @@ def progress_bar(cur, total):
 def progress_estimate(elapsed, timeleft):
     return "[Elapsed: " + str(int(elapsed)) + "m|Left: " + str(int(timeleft)) + "m]"
 
+
 def progress_total(total_elapsed):
     return "[Total elapsed: " + str(int(total_elapsed)) + "m]"
+
 
 def magick_status(source, i, file_string, elapsed, timeleft, total_elapsed):
     return file_string + " " + progress_bar(i, len(source)) + " " + progress_estimate(elapsed, timeleft) + " " + progress_total(total_elapsed)
 
-def load_fill_from_reader(source, current_file, total_files, t_start, filename):
+
+def load_fill_from_reader(source, t_start, file_string):
     i = 0
-    file_string = "[File \"" + filename + "\" " + str(current_file) + " out of " + str(total_files) + "]"
-    records = []
+    #file_string = "[File \"" + filename + "\" " + str(current_file) + " out of " + str(total_files) + "]"
     insert = sa.insert(Leak)
     database = sa.create_engine(
         SQLALCHEMY_DATABASE_URI,
         isolation_level="READ UNCOMMITTED")
     with database.begin() as connection:
         t = time.time()
-        for record in source:
-            records.append(record)
-            i += 1
-            if len(records) == 10000:
-                elapsed = (time.time() - t) / 60
-                timeleft = int(elapsed / i * len(source)) - elapsed
-                total_elapsed = (time.time() - t_start)/60
-                print magick_status(source, i, file_string, elapsed, timeleft, total_elapsed)
-                connection.execute(insert, records)
-                del records[:]
-        if records:
+        for records in source:
+            i += 1 * 10000
+            elapsed = (time.time() - t) / 60
+            timeleft = int(elapsed / i * len(source)) - elapsed
+            total_elapsed = (time.time() - t_start) / 60
+            print magick_status(source, i, file_string, elapsed, timeleft, total_elapsed)
+            # print "begin insert"
             connection.execute(insert, records)
+            # print "end insert"
     delta_t = time.time() - t
     print "File " + file_string + " imported in " + str(int(delta_t)) + " seconds"
     return delta_t, i
@@ -81,23 +117,9 @@ def load_fill_from_reader(source, current_file, total_files, t_start, filename):
 
 if __name__ == "__main__":
     start_time = time.time()
-    mypath = "/home/zlol/Documents/dumps/leaks/dumps/LinkedIn/"
+    mypath = "C:\\Users\\Zlooo\\Documents\\Exploit.in\\"
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    x=0
-    for ff in onlyfiles:
-        x+=1
-        with open(mypath+ff, "r") as f:
-            print "Current file: " + ff
-            source = []
-            for line in f:
-                try:
-                    #res = re.findall(r'(?P<email>.*@.*):(?P<password>.*)', line)
-                    res = re.findall(r':\s(?P<hash>.*)\s->\s(?P<email>.*@.*)', line)
-                    passw = res[0][0]
-                    if passw[0] == "x":
-                        passw = ""
-                    source.append(
-                        {'email': res[0][1], 'password_hash': passw, 'leak_source': 'linkedin_raw_2'})
-                except Exception as e:
-                    pass
-            load_fill_from_reader(source, x, len(onlyfiles), start_time, ff)
+    x = 0
+    for leak_file in onlyfiles:
+        load_fill_from_reader(LeaksIterator(
+            mypath + leak_file), start_time, leak_file)
